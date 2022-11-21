fa0b0266eb39f30bba2ad50a6a732bf4a0c8cd92
karol.szkola <karol.szkola@ezse.pl>
2022-11-21 14:25:36 +0100

    lekcja 21.11 kalkulator

---
+++ Pierwszy projekt/SimpleCalculatorApp/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace SimpleCalculatorApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFirstNumber = new System.Windows.Forms.Label();
+            this.labelSecoundNumber = new System.Windows.Forms.Label();
+            this.FirstNumber = new System.Windows.Forms.TextBox();
+            this.SecoundNumber = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSubstract = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelFirstNumber
+            // 
+            this.labelFirstNumber.AutoSize = true;
+            this.labelFirstNumber.Location = new System.Drawing.Point(56, 65);
+            this.labelFirstNumber.Name = "labelFirstNumber";
+            this.labelFirstNumber.Size = new System.Drawing.Size(91, 15);
+            this.labelFirstNumber.TabIndex = 0;
+            this.labelFirstNumber.Text = "Pierwsza Liczba:";
+            // 
+            // labelSecoundNumber
+            // 
+            this.labelSecoundNumber.AutoSize = true;
+            this.labelSecoundNumber.Location = new System.Drawing.Point(203, 65);
+            this.labelSecoundNumber.Name = "labelSecoundNumber";
+            this.labelSecoundNumber.Size = new System.Drawing.Size(75, 15);
+            this.labelSecoundNumber.TabIndex = 1;
+            this.labelSecoundNumber.Text = "Druga liczba:";
+            // 
+            // FirstNumber
+            // 
+            this.FirstNumber.Location = new System.Drawing.Point(56, 83);
+            this.FirstNumber.Name = "FirstNumber";
+            this.FirstNumber.Size = new System.Drawing.Size(100, 23);
+            this.FirstNumber.TabIndex = 2;
+            // 
+            // SecoundNumber
+            // 
+            this.SecoundNumber.Location = new System.Drawing.Point(203, 83);
+            this.SecoundNumber.Name = "SecoundNumber";
+            this.SecoundNumber.Size = new System.Drawing.Size(100, 23);
+            this.SecoundNumber.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(19, 148);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSubstract
+            // 
+            this.buttonSubstract.Location = new System.Drawing.Point(100, 148);
+            this.buttonSubstract.Name = "buttonSubstract";
+            this.buttonSubstract.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubstract.TabIndex = 5;
+            this.buttonSubstract.Text = "-";
+            this.buttonSubstract.UseVisualStyleBackColor = true;
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.Location = new System.Drawing.Point(192, 148);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(75, 23);
+            this.buttonMul.TabIndex = 6;
+            this.buttonMul.Text = "*";
+            this.buttonMul.UseVisualStyleBackColor = true;
+           
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.Location = new System.Drawing.Point(273, 148);
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.Size = new System.Drawing.Size(75, 23);
+            this.buttonDiv.TabIndex = 7;
+            this.buttonDiv.Text = "/";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(19, 214);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 15);
+            this.labelResult.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonDiv);
+            this.Controls.Add(this.buttonMul);
+            this.Controls.Add(this.buttonSubstract);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.SecoundNumber);
+            this.Controls.Add(this.FirstNumber);
+            this.Controls.Add(this.labelSecoundNumber);
+            this.Controls.Add(this.labelFirstNumber);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Prosty kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFirstNumber;
+        private System.Windows.Forms.Label labelSecoundNumber;
+        private System.Windows.Forms.TextBox FirstNumber;
+        private System.Windows.Forms.TextBox SecoundNumber;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSubstract;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
